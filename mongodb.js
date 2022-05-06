1e2e413c42d0c04846a557f85b9bde3f79c5fc60
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-05-06 06:19:37 +0530

    access denied

--- mongodb.js
+++ mongodb.js
@@ -1,11 +1,12 @@
 const mongoose = require(`mongoose`);
+require('dotenv').config()
 class mongo {
   constructor() {
     this.createMongoConnection();
   }
   createMongoConnection() {
     // mongoose.connect(`mongodb://localhost:27017/swiggy`);
-    mongoose.connect(`mongodb+srv://swiggy:HaljxyagASK0rcCy@cluster0.xszpp.mongodb.net/swiggy?retryWrites=true&w=majority`);
+    mongoose.connect(process.env.Mongo_url);
     mongoose.connection.once(`open`, () => {
       console.log(`MongoDB is connected`);
     });
